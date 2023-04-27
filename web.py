bd8f73eb54b0e759077bab31aa2bdd3fb199940d
AbsterZhu <thefrostzyt@gmail.com>
2023-04-27 20:27:03 +0800

    add examples

--- web.py
+++ web.py
@@ -13,6 +13,7 @@ from datetime import datetime
 title = """<h1 align="center">ChatCAD plus</h1>"""
 description = """**这是ChatCAD-plus的早期测试版本，欢迎任何反馈和贡献<br>-将胸片、牙片等图像上传至聊天框，即可获得ChatCAD-plus对该影像的分析<br>-可以继续与ChatCAD-plus交流，进一步了解可能的病症<br>-ChatCAD-plus会在必要的时候给出相关资料的链接**"""
 chatbot_bindings =  None
+chatbot = None
 
 
 def chatcad(history):
@@ -59,6 +60,13 @@ def add_state(info, history):
     history = history + [(None, response)]
     return history
 
+def clean_data():
+    return [(None, description)], None, None
+
+
+def example_img(i, history):
+    return i
+
 
 callback = gr.CSVLogger()
 
@@ -67,35 +75,59 @@ with gr.Blocks(css="""#col_container1 {margin-left: auto; margin-right: auto;}
                       #chatbot {height: 770px;}
                       #upload_btn {height: auto;}""") as demo:
     gr.HTML(title)
-
     with gr.Row():
-        with gr.Column(elem_id = "col_container1"):
-            chatbot = gr.Chatbot(value=[(None, description)], label="ChatCAD plus", elem_id='chatbot').style(height=700) #c
-    with gr.Row():
-        with gr.Column(elem_id = "col_container2", scale=0.85):
-            inputs = gr.Textbox(label="聊天框", placeholder="请输入文本或者上传图片") #t
-        with gr.Column(elem_id = "col_container2", scale=0.15, min_width=0):
+        with gr.Column(scale=0.2):
             with gr.Row():
-                btn = gr.UploadButton("📁", file_types=["file"], elem_id='upload_btn')
+                #top_p, temperature, top_k, repetition_penalty
+                with gr.Accordion("设置", open=True):
+                    with gr.Row():
+                        api_key_input = gr.Textbox(placeholder="请输入API key", label="API key")
+                    with gr.Row():
+                        api_key_submit = gr.Button("保存")
             with gr.Row():
-                inputs_submit = gr.Button("发送", elem_id='inputs_submit')
-            
-    with gr.Row():
-        #top_p, temperature, top_k, repetition_penalty
-        with gr.Accordion("设置", open=True):
+                gr.Markdown("### 请上传您想要咨询的医学图像   若您没有图像，可以下载下方的示例图像")
             with gr.Row():
-                api_key_input = gr.Textbox(placeholder="请输入API key", label="API key")
-                api_key_submit = gr.Button("保存")
+                upload_file = gr.UploadButton("📁上传图像", file_types=["file"], elem_id='upload_btn').style(size='lg')
+            with gr.Row():
+                img_i = gr.Image(show_label=False, type="numpy", interactive=False)
+                gr.Examples(
+                    [ 
+                        os.path.join(os.path.dirname(__file__), "imgs/examples/chest.jpg"),
+                        os.path.join(os.path.dirname(__file__), "imgs/examples/tooth.jpg"),
+                    ],
+                    img_i,
+                    img_i,
+                    example_img,
+                    label="示例图像"
+                )
+        with gr.Column(scale=0.8):
+            with gr.Row():
+                with gr.Column(elem_id = "col_container1"):
+                    chatbot = gr.Chatbot(value=[(None, description)], label="ChatCAD plus", elem_id='chatbot').style(height=700) #c
+            with gr.Row():
+                with gr.Column(elem_id = "col_container2", scale=0.85):
+                    inputs = gr.Textbox(label="聊天框", placeholder="请输入文本或者上传图片") #t
+                with gr.Column(elem_id = "col_container2", scale=0.15, min_width=0):
+                    with gr.Row():
+                        inputs_submit = gr.Button("发送", elem_id='inputs_submit')
+                    with gr.Row():
+                        clean_btn = gr.Button("清空", elem_id='clean_btn')
+                    
                 
     
     api_key_submit.click(add_state, [api_key_input, chatbot], [chatbot])
+    
     inputs_submit.click(add_text, [chatbot, inputs], [chatbot, inputs]).then(
         chatcad, chatbot, chatbot
     )
+    
+    clean_btn.click(clean_data, [], [chatbot, inputs, img_i])
+    
     inputs.submit(add_text, [chatbot, inputs], [chatbot, inputs]).then(
         chatcad, chatbot, chatbot
     )
-    btn.upload(add_file, [chatbot, btn], [chatbot]).then(
+    
+    upload_file.upload(add_file, [chatbot, upload_file], [chatbot]).then(
         chatcad, chatbot, chatbot
     )
     
